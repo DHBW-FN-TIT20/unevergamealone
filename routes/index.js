da656da29cdae39fd25cfe971015c111dbed7c5c
Florian Herkommer <florian.herkommer@gmx.de>
2022-02-02 11:57:03 +0000

    init

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'U Never Game Alone 2' });
-  res.sendFile("index.html");
+    // res.render('index', { title: 'U Never Game Alone 2' });
+    res.sendFile("index.html");
 });
 
 module.exports = router;
